b0a7e556a62708e052b9c9baacd74b42f9950656
Renato Mendes <renato.mendes@accenture.com>
2019-02-06 14:43:13 -0200

    Create tests for utils

--- GitHubAPI/Utils/ErrorView.swift
+++ GitHubAPI/Utils/ErrorView.swift
@@ -45,7 +45,7 @@ public final class ErrorView: UIView {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return nil
     }
 }
 
---
+++ GitHubAPITests/Utils/ErrorViewTests.swift
@@ -0,0 +1,49 @@
+//
+//  ErrorViewTests.swift
+//  GitHubAPITests
+//
+//  Created by renato.mendes on 06/02/19.
+//  Copyright © 2019 Renato Mendes. All rights reserved.
+//
+
+// swiftlint:disable implicitly_unwrapped_optional
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+@testable import GitHubAPI
+
+class ErrorViewTests: QuickSpec {
+
+    override func spec() {
+        describe("ErrorViewTests") {
+
+            var sut: ErrorView!
+
+            context("when you tried to instantiate HomeView with coder") {
+
+                beforeEach {
+                    let coder = NSCoder()
+                    sut = ErrorView(coder: coder)
+                }
+
+                it("should be expected a nil value") {
+                    expect(sut).to(beNil())
+                }
+            }
+
+            context("when load into screen") {
+
+                beforeEach {
+                    let frame = CGRect(x: 0, y: 0, width: 320, height: 300)
+                    sut = ErrorView(delegate: nil, frame: frame)
+                    sut.set(message: "Não achamos resultados")
+                }
+
+                it("should have the expected layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+        }
+    }
+}
